d3e8cdf8a093ffc98a8b052db32c10fba1ed4376
rits12b <77531655+rits12b@users.noreply.github.com>
2021-01-23 21:25:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -53,28 +53,7 @@ def submit():
 @app.route('/viewfeedback', methods=['GET','POST'])
 def viewfeedback():
     posts = Feedback.query.filter_by().all()
-    no_of_posts = 3
-    last = math.ceil(len(posts) / int(no_of_posts))
-    # [0: params['no_of_posts']]
-    # posts = posts[]
-    page = request.args.get('page')
-    if (not str(page).isnumeric()):
-        page = 1
-    page = int(page)
-    posts = posts[(page - 1) * int(no_of_posts): (page - 1) * int(no_of_posts) + int(no_of_posts)]
-    # Pagination Logic
-    # First
-    if (page == 1):
-        prev = "#"
-        next = "/?page=" + str(page + 1)
-    elif (page == last):
-        prev = "/?page=" + str(page - 1)
-        next = "#"
-    else:
-        prev = "/?page=" + str(page - 1)
-        next = "/?page=" + str(page + 1)
-
-    return render_template('feedback.html',posts=posts, prev=prev, next=next)
+    return render_template('feedback.html',posts=posts)
 
 
 if __name__ == '__main__':
